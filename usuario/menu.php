aa3a808986fd46d4c726d01963cae7cdc63fddfb
rjuanpablo <juanpabloruizbre@gmail.com>
2020-09-03 17:25:33 -0300

    Cambio de nombres a español

--- usuario/menu.php
+++ usuario/menu.php
@@ -3,23 +3,21 @@
 </div>
 <div id="user-nav" class="navbar navbar-inverse">
   <ul class="nav">
-    <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome User</span><b class="caret"></b></a>
+    <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Bienvenido Usuario</span><b class="caret"></b></a>
       <ul class="dropdown-menu">
-        <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
+        <li><a href="form-validation.php"><i class="icon-user"></i>Formulario</a></li>
         <li class="divider"></li>
       </ul>
     </li>
-    <li class=""><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+    <li class=""><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Salir</span></a></li>
   </ul>
 </div>
 <div id="sidebar">
   <ul>
-    <li><a href="index.php"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-    <li><a href="widgets.php"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li>
-    <li class="active"><a href="tables.php"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-    <li><a href="form-validation.php">Form with Validation</a></li>
-    <li><a href="calendar.html">Calendar</a></li>
-    <li><a href="grid.php">Grid</a></li>
-    <li><a href="invoice.php">Invoice</a></li>
+    <li><a href="index.php"><i class="icon icon-home"></i> <span>Administrador</span></a> </li>
+    <li class="active"><a href="widgets.php"><i class="icon icon-th"></i> <span>Usuario</span></a></li>
+    <li><a href="calendar.html">Calendario</a></li>
+    <li><a href="grid.php">Planes</a></li>
+    <li><a href="invoice.php">Factura</a></li>
   </ul>
 </div>
